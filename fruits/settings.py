3a3c1b2610fe4695bbffea93f7ec594621b654a2
daniellima338 <daniellima338@gmail.com>
2022-03-02 20:28:36 +0000

    secret key drawn from enviroment

--- fruits/settings.py
+++ fruits/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vl*zy097_o)ne17_5n-5rq+nog##$0=)p+jnky*)x$+%do#p(z'
+SECRET_KEY = os.environ.get('SECRET_KEY_FRUITS', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
